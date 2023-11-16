f2a68172ddebfc46c8ae1797acb9eaa102e67835
Cameron Woodard <cwoodard@bayviewglen.ca>
2023-11-16 09:27:49 -0500

    its all working

---
+++ sportssite/script.js
@@ -0,0 +1,8 @@
+
+function onButtonClick() {
+    let elementName = ""; // fill later
+    element = document.getElementById(elementName);
+
+    
+    localStorage.setItem("teams",["blue jays",""]);
+}
